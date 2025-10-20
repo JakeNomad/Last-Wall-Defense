e909a7cfe00fe61c9265ae42150eb1fecd8dbcdc
Mustafa Emir <emiruzgar2@gmail.com>
2025-10-20 20:12:53 +0300

    Camera Movement
    Camera now follows the player

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    [Header("Elements")]
+    public GameObject player;
+
+    [Header("Settings")]
+    private float offsetX = -0.64f;
+    private float offsetY = -1.12f;
+    private float offsetZ = 6.01f;
+    
     void Start()
     {
-        
+        // Vector3 playerToCamera = (player.transform.position - transform.position);
+        // Debug.Log(playerToCamera);
+        // -0.64, -1.12 , 6.01
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 offset = new Vector3(offsetX, offsetY, offsetZ);
+        transform.position = player.transform.position - offset;
     }
 }
